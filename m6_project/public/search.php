290d29f71529bd7e5bf0cd963b8cddb8d2632284
Awes <28003@ma-web.nl>
2023-12-04 14:46:57 +0100

    forms 1e opdracht en weeromstandigheden afgemaakt

---
+++ m6_project/public/search.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Search</title>
+    <script defer type="text/javascript" src="assets/js/app.js"></script>
+  </head>
+  <body>
+    <form action="/search.php">
+      <label for="firstname">First Name:</label><br />
+      <input type="text" id="firstname" name="firstname" value="John" /><br />
+      <label for="search"></label>
+      <input type="text" id="search" name="search" />
+      <input type="submit" />
+    </form>
+  </body>
+</html>
