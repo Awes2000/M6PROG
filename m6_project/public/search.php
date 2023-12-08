fd2e848d77e9c754b1ad7eb3d009c80d1f03e972
Awes <28003@ma-web.nl>
2023-12-08 12:29:48 +0100

    03superglobals

--- m6_project/public/search.php
+++ m6_project/public/search.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Search</title>
-    <script defer type="text/javascript" src="assets/js/app.js"></script>
-  </head>
-  <body>
-    <form action="/search.php">
-      <label for="firstname">First Name:</label><br />
-      <input type="text" id="firstname" name="firstname" value="John" /><br />
-      <label for="search"></label>
-      <input type="text" id="search" name="search" />
-      <input type="submit" />
-    </form>
-  </body>
-</html>
+<?php
+echo '<pre>';
+var_dump( $_GET['zoeken'] );
+var_dump( $_GET['naam'] );
+echo '</pre>';
