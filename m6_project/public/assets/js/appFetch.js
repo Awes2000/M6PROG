9cf551db6d0cdf8e1476cd61c13bbe3df1c01284
Awes <28003@ma-web.nl>
2023-12-15 12:16:59 +0100

    3.04 post versturen af

--- m6_project/public/assets/js/appFetch.js
+++ m6_project/public/assets/js/appFetch.js
@@ -1,5 +1,12 @@
 let getForm = document.getElementById("getForm");
 
+let postForm = document.getElementById("postForm");
+
+postForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  toPhpwithPost(event);
+});
+
 function toPhpwithget(event) {
   let form = event.target;
   const data = new FormData(form);
@@ -14,6 +21,31 @@ function toPhpwithget(event) {
   fetch(url).then((response) => {
     console.log(response);
   });
+}
+
+function toPhpwithPost(event) {
+  let form = event.target;
+  let jsonForm = FormToDictionary(form);
+  console.log(jsonForm);
+  let options = {
+    method: "POST",
+    cache: "no-cache",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(jsonForm),
+  };
+  fetch("fetchPost.php", options).then(async (response) => {
+    console.log(response);
+  });
+  console.log(options);
+}
+
+function FormToDictionary(form) {
+  const data = new FormData(form);
+  let formKeyValue = {};
+  for (const [name, value] of data) {
+    formKeyValue[name] = value;
+  }
+  return formKeyValue;
 }
 
 getForm.addEventListener("submit", (event) => {
