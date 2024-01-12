fd0dbe4476341e7b55e62f0dea4e2c8a3b72a4f7
Awes <28003@ma-web.nl>
2024-01-12 15:53:19 +0100

    4.04 display data

--- m6_project/public/assets/js/app2.js
+++ m6_project/public/assets/js/app2.js
@@ -12,15 +12,30 @@ function searchPersoon(event) {
   event.preventDefault();
   let form = event.target;
   const data = new FormData(form);
-  let url = "search.php?search=" + data.get("searchPersoon");
+  let url = "searchNaw.php?searchPersoon=" + data.get("searchPersoon");
   console.log(url);
 
   fetch(url).then(async (response) => {
     console.log(response);
     let json = await response.json();
     console.log(json);
+    ShowPersoon(json);
   });
 
   // Implementeer hier de logica om de formuliergegevens te verwerken
   // ...
+}
+
+function ShowPersoon(json) {
+  let person = json[0]; // Aanname: de eerste persoon in de lijst is degene die we willen tonen
+
+  // Update de inhoud van de paragrafen
+  document.getElementById("naam").textContent = "Naam: " + person.naam;
+  document.getElementById("straat").textContent = "Straat: " + person.straat;
+  document.getElementById("huisnummer").textContent =
+    "Huisnummer: " + person.huisnummer;
+  document.getElementById("postcode").textContent =
+    "Postcode: " + person.postcode;
+  document.getElementById("email").textContent = "Email: " + person.email;
+  // Voeg meer toe indien nodig
 }
