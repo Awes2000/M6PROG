d86bd9ab2370ffb6c11d59e1e3384177aa7f148a
Awes <28003@ma-web.nl>
2024-01-14 16:50:57 +0100

    4.07 search-like af

--- m6_project/public/search.php
+++ m6_project/public/search.php
@@ -1,16 +1,20 @@
 <?php
 require_once '../source/config.php';
 require_once SOURCE_ROOT . 'database.php';
-$connection = database_connect();
-$plaats = $_GET["plaats"];
 
-$sql = 'SELECT * FROM weeromstandigheden WHERE Plaats=? ORDER BY Datum'; 
+$connection = database_connect();
+
+$plaats = '';
+if (isset($_GET["plaats"])) {
+    $plaats = '%' . $connection->real_escape_string($_GET["plaats"]) . '%';
+}
+
+$sql = 'SELECT * FROM weeromstandigheden WHERE Plaats LIKE ? ORDER BY Datum'; 
 $stmt = $connection->prepare($sql);
 $stmt->bind_param('s', $plaats);
 $stmt->execute();
 $result = $stmt->get_result();
-while ($weersomstandigheden = mysqli_fetch_assoc($result)){
-var_dump( $weersomstandigheden );
+
+while ($weersomstandigheden = mysqli_fetch_assoc($result)) {
+    var_dump($weersomstandigheden);
 };
-
-
