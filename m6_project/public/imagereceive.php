97283e128ff11d21149b73261ff795bee6e24c1d
Awes <28003@ma-web.nl>
2024-01-19 13:22:02 +0100

    6.01 uniek bestand

--- m6_project/public/imagereceive.php
+++ m6_project/public/imagereceive.php
@@ -1,5 +1,32 @@
 <?php
 
+function handleFile($file) {
+    $response = ["succeeded" => false, "message" => ""];
+    
+    // Genereer een unieke bestandsnaam en stel het pad in
+    $link = uniqid();
+    $location = $file["tmp_name"];
+    $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+    $filename = "../uploads/" . $link . "." . $ext;
 
-print_r($_FILES);
-move_uploaded_file($_FILES["image"]["tmp_name"],"../uploads/temp.png");
+    // Verplaats de afbeelding naar de 'uploads'-map
+    if (move_uploaded_file($location, $filename)) {
+        $response["succeeded"] = true;
+        $response["message"] = "Upload successful";
+    } else {
+        $response["message"] = "Error during upload";
+    }
+
+    return $response;
+}
+
+// Controleer of er een bestand is geüpload
+if ($_FILES && $_FILES["image"]["error"] === 0) {
+    $response = handleFile($_FILES["image"]);
+} else {
+    $response = ["succeeded" => false, "message" => "No file uploaded or error during upload"];
+}
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($response);
+
